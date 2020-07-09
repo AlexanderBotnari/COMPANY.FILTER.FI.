6417785e19f7862474f2d882c29d9404c4507593
AlexanderBotnari <63365662+AlexanderBotnari@users.noreply.github.com>
2020-07-09 13:07:28 +0300

    Update Employee.java

--- src/Employee.java
+++ src/Employee.java
@@ -1,41 +1,18 @@
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+//@ToString
 public class Employee {
 	
-    private String fullName;
+	private String fullName;
     private int salary;
     private int experience;
     
-	public Employee(String fullName, int salary, int experience) {
-		this.fullName = fullName;
-		this.salary = salary;
-		this.experience = experience;
-	}
-
-	public String getFullName() {
-		return fullName;
-	}
-
-	public void setFullName(String fullName) {
-		this.fullName = fullName;
-	}
-
-	public int getSalary() {
-		return salary;
-	}
-
-	public void setSalary(int salary) {
-		this.salary = salary;
-	}
-
-	public int getExperience() {
-		return experience;
-	}
-
-	public void setExperience(int experience) {
-		this.experience = experience;
-	}
-
-
 	@Override
 	public String toString() {
 		return fullName+"  "+ salary + "$"+"  "+ experience + " years";
